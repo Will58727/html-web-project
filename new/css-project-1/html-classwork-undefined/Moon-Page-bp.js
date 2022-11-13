e7a10edd25445d0ebfadbe23d8a20f641810f666
Will58727 <william.powell501@yahoo.com>
2022-11-12 19:00:36 -0600

    Moon-Page

---
+++ new/css-project-1/html-classwork-undefined/Moon-Page-bp.js
@@ -0,0 +1,20 @@
+const folderpath = "///C:/Users/will/OneDrive/Documents/GitHub/html-web-project/new/css-project-1/html-classwork-undefined/images/";
+let image = document.getElementById("moonImage");
+function fullMoon() {
+    let image = document.getElementById("moonImage");
+    image.src = folderpath + "Full-Moon.jpg"
+    
+}
+
+function bloodMoon() {
+    let image = document.getElementById("moonImage");
+    image.src = folderpath + "Blood-Moon.jpg"
+    
+}
+
+function eclipse() {
+    let image = document.getElementById("moonImage");
+    image.src = folderpath + "eclipse.jpg"
+    
+    
+}
